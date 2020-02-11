56808bba3a2ed4edb617c663d1be6fe4fb4aa777
Yuanhan Chen <mainemayjane@gmail.com>
2020-02-10 21:57:39 -0800

    Update and rename 175.Combine Two Tables to 175.Combine Two Tables.sql

--- 175.Combine Two Tables.sql
+++ 175.Combine Two Tables.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 SELECT A.City, A.State, P.FirstName, P.LastName FROM Address A
-RIGHT JOIN Person P ON A.PersonId = P.PersonId
+       RIGHT JOIN Person P ON A.PersonId = P.PersonId
